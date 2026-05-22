e0be2390fe6c3e0e2059479daada18bcb2e31aa9
jeelbelani12-star <jeelbelani12@gmail.com>
2026-05-22 21:54:46 +0530

    update button

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -19,6 +19,10 @@ export default function TextForm(props) {
         setText(event.target.value);
     }
 
+    const handleClearClick = () => {
+        setText('');
+    }
+
     const handleCopy = () => {
         navigator.clipboard.writeText(text);
         props.showAlert("Copied to Clipboard!", "success");
@@ -34,6 +38,7 @@ export default function TextForm(props) {
         <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
         <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
         <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
     </div>
     <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
         <h2>Your text summary</h2>
